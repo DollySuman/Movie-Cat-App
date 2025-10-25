cdd2f6877bf6533f9f773400b87f4b9242025e93
Dolly Suman <dolly.suman.023@gmail.com>
2025-10-25 13:25:06 +0530

    movies added

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-const movies = ["Frozen2","Toy Story 3","Iron Man 2"]
+// const movies = ["Frozen2","Toy Story 3","Iron Man 2"]
 const movies = [{
     name: "Frozen 2",
     desc: "About Frozen 2",
-    img: ""
-}]
+    img: "https://upload.wikimedia.org/wikipedia/en/8/89/Frozen_II_%282019_animated_film%29.jpg"
+},
+
+{
+    name: "Barbie",
+    desc: "A great movie",
+    img: "https://images-cdn.ubuy.co.in/660f9e73afccd7049644a2bc-poster-2023-movie-posters-prints-bedroom.jpg"
+},
+
+{
+    name: "Zootopia",
+    desc: "A movie for kids",
+    img: "https://m.media-amazon.com/images/I/71hWkxTBHRL.jpg"
+}
+
+
+]
 
 const title = () =>{
     movies.forEach(movie =>{
@@ -12,14 +27,14 @@ const title = () =>{
         
         document.querySelector(".head").innerHTML += `<div class="card">
         <div class="moviename">
-        <h1>${movie}</h1>
-        <p>Cars 2 was Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe repellendus minus consequuntur, enim sed corporis autem rem necessitatibus ut dolores exercitationem sunt fuga culpa?
-        
+        <h1>${movie.name}</h1>
+        <p>
+        ${movie.desc}
         </p>
         </div>
         
         <div class="image">
-        <img  src="https://c8.alamy.com/comp/HXJR8W/release-date-june-2017-title-cars-3-studio-pixar-director-brian-fee-HXJR8W.jpg" alt="poster">
+        <img  src=${movie.img} alt="poster">
         </div>
         </div>`
         })
@@ -28,3 +43,6 @@ const title = () =>{
     title();
 
     console.log(movies)
+
+
+    //Dynamically add karna seekh gyi ab you have to learn how to handle apis
