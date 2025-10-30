57ed8ef20f01a04a2f585407f4f0371560a430f4
Dolly Suman <dolly.suman.023@gmail.com>
2025-10-30 21:02:13 +0530

    dynamic

--- api.js
+++ api.js
@@ -1,25 +1,67 @@
-fetch("https://imdb.iamidiotareyoutoo.com/search?q=Iron%20Man")
-.then(res => res.json())
-.then(data => {
-    data.description.forEach(movie => {
-        console.log(movie["#TITLE"]);
-        document.querySelector(".head").innerHTML += `<div class="card">
-                <div class="moviename">
-                    <h1>${movie["#TITLE"]}</h1>
-                    <h2> Starring ${movie["#ACTORS"]}</h2>
-                    <h4>Released in the Year - ${movie["#YEAR"]}</h4>
-                    <h6>The IMDB rank of this movie is : ${movie["#RANK"]}</h6>
+// fetch("https://imdb.iamidiotareyoutoo.com/search?q=Iron%20Man")
+// .then(res => res.json())
+// .then(data => {
+//     data.description.forEach(movie => {
+//         console.log(movie["#TITLE"]);
+//         document.querySelector(".head").innerHTML += `<div class="card">
+//                 <div class="moviename">
+//                     <h1>${movie["#TITLE"]}</h1>
+//                     <h2> Starring ${movie["#ACTORS"]}</h2>
+//                     <h4>Released in the Year - ${movie["#YEAR"]}</h4>
+//                     <h6>The IMDB rank of this movie is : ${movie["#RANK"]}</h6>
                     
-                    <h2><a href="${movie["#IMDB_URL"]}" target = "_blank" >Visit IMDB</a></h2>
+//                     <h2><a href="${movie["#IMDB_URL"]}" target = "_blank" >Visit IMDB</a></h2>
                     
                         
-                    </p>
-                </div>
+//                     </p>
+//                 </div>
                 
+//             </div>`
+        
+//             // Also make buttons and add event listeners to them
+            
+//             // Next step is you learn to create your own api with images of the movie and then make a movie datanase 
+//     });
+// })
+
+
+   document.getElementById("search").addEventListener("click",getMovie)
+   document.getElementById("inp").addEventListener("keypress", e => {
+    if(e.key === "Enter"){
+        getMovie();
+    }
+   })
+
+function getMovie() {
+   const mname =  document.getElementById("inp").value.trim();
+
+
+const url = `https://imdb.iamidiotareyoutoo.com/search?q=${mname}`
+
+fetch(url)
+.then(res=> res.json())
+.then(data => {
+    console.log(data)
+    data.description.forEach(movie => {
+
+        document.querySelector(".head").innerHTML += `<div class="card">
+                <div class="moviename">
+                    <h1>${movie["#AKA"]}</h1>
+                    <p> Starring ${movie["#ACTORS"]} </p>
+                    <div><a href= "${movie["#IMDB_URL"]}" target = "_blank " > Watch Now </a></div>
+                </div>
+
+                
+                
+                 
+                <div class="image">
+                    <img  src="${movie["#IMG_POSTER"]}" alt="poster">
+                </div>
             </div>`
         
-            // Also make buttons and add event listeners to them
-            
-            // Next step is you learn to create your own api with images of the movie and then make a movie datanase 
     });
 })
+
+}
+
+// Now next step is to remove the previous cards why nuw movie name is entered and search moviess other than Iron man
