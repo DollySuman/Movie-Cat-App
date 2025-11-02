c41be54804ed86224d56bf97496ecbe8832de219
Dolly Suman <dolly.suman.023@gmail.com>
2025-11-02 14:36:39 +0530

    new changes

--- api.js
+++ api.js
@@ -1,28 +1,4 @@
-// fetch("https://imdb.iamidiotareyoutoo.com/search?q=Iron%20Man")
-// .then(res => res.json())
-// .then(data => {
-//     data.description.forEach(movie => {
-//         console.log(movie["#TITLE"]);
-//         document.querySelector(".head").innerHTML += `<div class="card">
-//                 <div class="moviename">
-//                     <h1>${movie["#TITLE"]}</h1>
-//                     <h2> Starring ${movie["#ACTORS"]}</h2>
-//                     <h4>Released in the Year - ${movie["#YEAR"]}</h4>
-//                     <h6>The IMDB rank of this movie is : ${movie["#RANK"]}</h6>
-                    
-//                     <h2><a href="${movie["#IMDB_URL"]}" target = "_blank" >Visit IMDB</a></h2>
-                    
-                        
-//                     </p>
-//                 </div>
-                
-//             </div>`
-        
-//             // Also make buttons and add event listeners to them
-            
-//             // Next step is you learn to create your own api with images of the movie and then make a movie datanase 
-//     });
-// })
+
 
 
    document.getElementById("search").addEventListener("click",e=>{
@@ -73,4 +49,3 @@ fetch(url)
 
 }
 
-// Now next step is to remove the previous cards why nuw movie name is entered and search moviess other than Iron man
